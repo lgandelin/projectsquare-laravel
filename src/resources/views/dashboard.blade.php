9bf213bdcd90cabae65b90d8046dfe25020083c3
Louis Gandelin <lgandelin@web-access.fr>
2016-01-20 09:10:19 +0100

    Added projects list

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -1,14 +1,22 @@
 @extends('gateway::app')
 
 @section('content')
-<div class="container-flui">
+<div class="container-fluid">
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
 			<div class="panel panel-default">
 				<div class="panel-heading">Home</div>
 
 				<div class="panel-body">
+                    <h3>{{ $user->first_name }} {{ $user->last_name }}</h3>
 
+                    <ul>
+                        @foreach ($user->projects as $project)
+                            <li>
+                                {{ $project->client->name }} - {{ $project->name }}
+                            </li>
+                        @endforeach
+                    </ul>
 				</div>
 			</div>
 		</div>
