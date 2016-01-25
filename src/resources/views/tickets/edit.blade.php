fb8d2066a1a92ebcf49b496cdba1cf6dbc744b55
Louis Gandelin <lgandelin@web-access.fr>
2016-01-25 18:14:06 +0100

    Split ticket update action in 2 separate actions

--- src/resources/views/tickets/edit.blade.php
+++ src/resources/views/tickets/edit.blade.php
@@ -23,7 +23,7 @@
         </div>
     @endif
 
-    <form action="{{ route('tickets_update') }}" method="post">
+    <form action="{{ route('tickets_update_infos') }}" method="post">
         <div class="row">
             <div class="col-md-6">
                 <div class="form-group">
@@ -46,8 +46,70 @@
                 </div>
 
                 <div class="form-group">
+                    <input type="submit" class="btn btn-success" value="{{ trans('gateway::generic.valid') }}" />
+                    <a href="{{ route('tickets_index') }}" class="btn btn-default">{{ trans('gateway::generic.back') }}</a>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="type_id">{{ trans('gateway::tickets.type') }}</label>
+                    @if (isset($ticket_types))
+                        <select class="form-control" name="type_id">
+                            <option value="">{{ trans('gateway::generic.choose_value') }}</option>
+                            @foreach ($ticket_types as $ticket_type)
+                                <option value="{{ $ticket_type->id }}" @if (isset($ticket) && $ticket->type_id == $ticket_type->id)selected="selected"@endif>{{ $ticket_type->name }}</option>
+                            @endforeach
+                        </select>
+                    @else
+                        <div class="info bg-info">{{ trans('gateway::tickets.no_ticket_type_yet') }}</div>
+                    @endif
+                </div>
+
+                <div class="form-group">
                     <label for="description">{{ trans('gateway::tickets.description') }}</label>
                     <textarea class="form-control" placeholder="{{ trans('gateway::tickets.description') }}" name="description">@if (isset($ticket->description)){{ $ticket->description }}@endif</textarea>
+                </div>
+            </div>
+        </div>
+
+        @if (isset($ticket->id))
+            <input type="hidden" name="ticket_id" value="{{ $ticket->id }}" />
+        @endif
+
+        {!! csrf_field() !!}
+    </form>
+
+    <hr/>
+
+    <form action="{{ route('tickets_update') }}" method="post">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label for="status_id">{{ trans('gateway::tickets.status') }}</label>
+                    @if (isset($ticket_status))
+                    <select class="form-control" name="status_id">
+                        <option value="">{{ trans('gateway::generic.choose_value') }}</option>
+                        @foreach ($ticket_status as $ticket_status)
+                        <option value="{{ $ticket_status->id }}" @if (isset($ticket) && isset($ticket->states[0]) && $ticket->states[0]->status_id == $ticket_status->id)selected="selected"@endif>{{ $ticket_status->name }}</option>
+                        @endforeach
+                    </select>
+                    @else
+                    <div class="info bg-info">{{ trans('gateway::tickets.no_ticket_status_yet') }}</div>
+                    @endif
+                </div>
+
+                <div class="form-group">
+                    <label for="allocated_user_id">{{ trans('gateway::tickets.allocated_user') }}</label>
+                    @if (isset($users))
+                    <select class="form-control" name="allocated_user_id">
+                        <option value="">{{ trans('gateway::generic.choose_value') }}</option>
+                        @foreach ($users as $user)
+                        <option value="{{ $user->id }}" @if (isset($ticket) && isset($ticket->states[0]) && $ticket->states[0]->allocated_user_id == $user->id)selected="selected"@endif>{{ $user->first_name }} {{ $user->last_name }}</option>
+                        @endforeach
+                    </select>
+                    @else
+                    <div class="info bg-info">{{ trans('gateway::tickets.no_user_yet') }}</div>
+                    @endif
                 </div>
 
                 <div class="form-group">
@@ -58,50 +120,8 @@
 
             <div class="col-md-6">
                 <div class="form-group">
-                    <label for="type_id">{{ trans('gateway::tickets.type') }}</label>
-                    @if (isset($ticket_types))
-                        <select class="form-control" name="type_id">
-                            <option value="">{{ trans('gateway::generic.choose_value') }}</option>
-                            @foreach ($ticket_types as $ticket_type)
-                            <option value="{{ $ticket_type->id }}" @if (isset($ticket) && $ticket->type_id == $ticket_type->id)selected="selected"@endif>{{ $ticket_type->name }}</option>
-                            @endforeach
-                        </select>
-                    @else
-                        <div class="info bg-info">{{ trans('gateway::tickets.no_ticket_type_yet') }}</div>
-                    @endif
-                </div>
-
-                <div class="form-group">
                     <label for="title">{{ trans('gateway::tickets.due_date') }}</label>
                     <input class="form-control datepicker" type="text" placeholder="{{ trans('gateway::tickets.due_date_placeholder') }}" name="due_date" @if (isset($ticket->due_date))value="{{ $ticket->due_date }}"@endif autocomplete="off" />
-                </div>
-
-                <div class="form-group">
-                    <label for="status_id">{{ trans('gateway::tickets.status') }}</label>
-                    @if (isset($ticket_status))
-                        <select class="form-control" name="status_id">
-                            <option value="">{{ trans('gateway::generic.choose_value') }}</option>
-                            @foreach ($ticket_status as $ticket_status)
-                            <option value="{{ $ticket_status->id }}" @if (isset($ticket) && isset($ticket->states[0]) && $ticket->states[0]->status_id == $ticket_status->id)selected="selected"@endif>{{ $ticket_status->name }}</option>
-                            @endforeach
-                        </select>
-                    @else
-                        <div class="info bg-info">{{ trans('gateway::tickets.no_ticket_status_yet') }}</div>
-                    @endif
-                </div>
-
-                <div class="form-group">
-                    <label for="allocated_user_id">{{ trans('gateway::tickets.allocated_user') }}</label>
-                    @if (isset($users))
-                        <select class="form-control" name="allocated_user_id">
-                            <option value="">{{ trans('gateway::generic.choose_value') }}</option>
-                            @foreach ($users as $user)
-                            <option value="{{ $user->id }}" @if (isset($ticket) && isset($ticket->states[0]) && $ticket->states[0]->allocated_user_id == $user->id)selected="selected"@endif>{{ $user->first_name }} {{ $user->last_name }}</option>
-                            @endforeach
-                        </select>
-                    @else
-                        <div class="info bg-info">{{ trans('gateway::tickets.no_user_yet') }}</div>
-                    @endif
                 </div>
 
                 <div class="form-group">
@@ -114,20 +134,22 @@
                     </select>
                 </div>
             </div>
+
+            @if (isset($ticket->id))
+                <input type="hidden" name="ticket_id" value="{{ $ticket->id }}" />
+            @endif
+
+            @if (isset($logged_in_user))
+                <input type="hidden" name="author_user_id" value="{{ $logged_in_user->id }}" />
+            @endif
+
+            {!! csrf_field() !!}
         </div>
-
-        @if (isset($ticket->id))
-            <input type="hidden" name="ticket->id" value="{{ $ticket->id }}" />
-        @endif
-
-        @if (isset($logged_in_user))
-            <input type="hidden" name="author_user_id" value="{{ $logged_in_user->id }}" />
-        @endif
-
-        {!! csrf_field() !!}
     </form>
 
-    <h2>{{ trans('gateway::tickets.ticket_history') }}</h2>
+    <p>&nbsp;</p>
+
+    <h2 class="page-header">{{ trans('gateway::tickets.ticket_history') }}</h2>
     <table class="table table-striped table-bordered">
         <thead>
             <th>{{ trans('gateway::generic.creation_date') }}</th>
