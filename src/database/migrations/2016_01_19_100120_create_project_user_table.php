b1ece6c4c237185c25ba8347ade37aac24f70f84
Louis Gandelin <lgandelin@web-access.fr>
2016-01-26 10:34:14 +0100

    Fixed a bug in user_projects migration file

--- src/database/migrations/2016_01_19_100120_create_project_user_table.php
+++ src/database/migrations/2016_01_19_100120_create_project_user_table.php
@@ -15,6 +15,7 @@ class CreateProjectUserTable extends Migration
         Schema::create('user_projects', function (Blueprint $table) {
             $table->integer('user_id');
             $table->integer('project_id');
+            $table->integer('role_id');
         });
     }
 
