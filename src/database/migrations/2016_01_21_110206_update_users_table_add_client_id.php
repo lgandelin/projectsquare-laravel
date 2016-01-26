db38b290a7fb9a1626b3b3e9e41537191cd3970d
Louis Gandelin <lgandelin@web-access.fr>
2016-01-26 11:49:30 +0100

    Fixed a bug in client migration file

--- src/database/migrations/2016_01_21_110206_update_users_table_add_client_id.php
+++ src/database/migrations/2016_01_21_110206_update_users_table_add_client_id.php
@@ -13,7 +13,7 @@ class UpdateUsersTableAddClientId extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('client_id')->after('password');
+            $table->string('client_id')->after('password')->nullable();
         });
     }
 
