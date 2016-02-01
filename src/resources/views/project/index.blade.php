b3e84d5c894e75e247113686ad99cb5429593b1a
Louis Gandelin <lgandelin@web-access.fr>
2016-02-01 11:13:51 +0100

    Added Agency folder in Controllers

--- src/resources/views/project/index.blade.php
+++ src/resources/views/project/index.blade.php
@@ -3,10 +3,12 @@
 @section('content')
     <ol class="breadcrumb">
         <li><a href="{{ route('dashboard') }}">Tableau de bord</a></li>
-        <li class="active">Projets</li>
+        <li class="active">{{ $project->name }}</li>
     </ol>
 
     <div class="page-header">
-        <h1>Projets</h1>
+        <h1>{{ $project->name }}</h1>
+
+
     </div>
 @endsection
