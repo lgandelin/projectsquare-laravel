8287ca1566dc657788af2661ce6f76da30e66da9
Blandine Cortes <bcortes@web-access.fr>
2016-02-02 18:08:29 +0100

    Continued to do page

--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -12,24 +12,35 @@
 
     <div class="col-md-12">
         <div class="block">
-            <div class="block-content"></div>
-            <form method="post" action="{{ route('to_do_store') }}">
-                <label for="name">Nouvelle tâche</label> : <input type="text" name="name" id="name"/>
-                <div class="sent">
-                    <input type="submit" value="Ajouter" />
-                </div>
-                {!! csrf_field() !!}
-            </form>
-            <ul>
-                @foreach ($tasks as $task)
-                    <li>
-                        @if ($task->status == true)<del>@endif
-                        {{ $task->name }}
-                        @if ($task->status == true)</del>@endif
-                    </li>
+            <div class="block-content">
+                <form method="post" action="{{ route('to_do_store') }}">
+                    <label for="name">Nouvelle tâche</label> : <input type="text" name="name" id="name"/>
+                    <div class="sent">
+                        <input type="submit" value="Ajouter" />
+                    </div>
+                    {!! csrf_field() !!}
+                </form>
+                <ul>
+                    @foreach ($tasks as $task)
+                        <li>
+                            <span class="@if($task->status == true)task-status-completed @endif">{{ $task->name }}</span>
+                            <div>
+                                @if($task->status == true)
+                                    <a href="{{ route('to_do_update', ['id' => $task->id]) }}">Ouvrir</a>
+                                 @else
+                                    <a href="{{ route('to_do_update', ['id' => $task->id]) }}">Terminer</a>
+                                @endif
+                            </div>
 
-                @endforeach
-            </ul>
+
+
+
+                            <a href="{{ route('to_do_delete', ['id' => $task->id]) }}">Supprimer</a>
+                        </li>
+
+                    @endforeach
+                </ul>
+            </div>
 
 
         </div>
