41762d7227fdf3d06d50130d31fcbaa189f88a8b
Blandine Cortes <bcortes@web-access.fr>
2016-02-04 17:04:47 +0100

    Continued to do page

--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -5,42 +5,45 @@
         <li><a href="{{ route('dashboard') }}">Tableau de bord</a></li>
         <li class="active">Tickets</li>
     </ol>
-
+    
     <div class="page-header">
         <h1>Todo</h1>
     </div>
 
-    <div class="col-md-12">
-        <div class="block">
-            <div class="block-content">
-                <form method="post" action="{{ route('to_do_store') }}">
-                    <label for="name">Nouvelle tâche</label> : <input type="text" name="name" id="name"/>
-                    <div class="sent">
-                        <input type="submit" value="Ajouter" />
-                    </div>
-                    {!! csrf_field() !!}
-                </form>
-                <ul>
-                    @foreach ($tasks as $task)
-                        <li>
-                            <span class="name @if($task->status == true)task-status-completed @endif">{{ $task->name }}</span>
-                            <div>
-                                <form method="post" action="{{ route('to_do_update') }}">
-                                    <input class="status" type="checkbox" name="status" @if($task->status == true) checked @endif>
-                                    <input type="hidden" name="id" value="{{ $task->id }}" />
-                                    {!! csrf_field() !!}
-                                </form>
-                            </div>
+    <div class="task-template">
+
+        <div class="col-md-12">
+            <div class="block">
+                <div class="block-content">
+                    <form method="post" action="{{ route('to_do_store') }}">
+                        <label for="name">Nouvelle tâche</label> : <input type="text" class="form-control new-task"  name="name" id="name"/>
+                        <div class="sent">
+                            <input type="submit" class="btn btn-success" value="Ajouter" />
+                        </div>
+                        {!! csrf_field() !!}
+                    </form>
+                    <ul class="tasks">
+                        @foreach ($tasks as $task)
+                            <li class="task">
+                                <span class="name @if($task->status == true)task-status-completed @endif">{{ $task->name }}</span>
+                                <div>
+                                    <form method="post" action="{{ route('to_do_update') }}">
+                                        <input class="status" type="checkbox" name="status" @if($task->status == true) checked @endif>
+                                        <input type="hidden" name="id" value="{{ $task->id }}" />
+                                        {!! csrf_field() !!}
+                                    </form>
+                                </div>
 
 
-                            <a href="{{ route('to_do_delete', ['id' => $task->id]) }}">Supprimer</a>
-                        </li>
+                                <a href="{{ route('to_do_delete', ['id' => $task->id]) }}">Supprimer</a>
+                            </li>
 
-                    @endforeach
-                </ul>
+                        @endforeach
+                    </ul>
+                </div>
+
+
             </div>
-
-
         </div>
     </div>
 
