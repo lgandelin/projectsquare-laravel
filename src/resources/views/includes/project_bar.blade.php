d56042458d548fc134171cf198c629d07cadc09a
Louis Gandelin <lgandelin@web-access.fr>
2016-02-08 17:30:56 +0100

    Added key numbers and loading times

--- src/resources/views/includes/project_bar.blade.php
+++ src/resources/views/includes/project_bar.blade.php
@@ -4,9 +4,9 @@
             <li @if ($active == 'index')class="active"@endif><a href="{{ route('project_index', ['id' => $project->id]) }}">{{ trans('gateway::project.summary') }}</a></li>
             <li @if ($active == 'cms')class="active"@endif><a href="{{ route('project_cms', ['id' => $project->id]) }}">{{ trans('gateway::project.cms') }}</a></li>
             <li @if ($active == 'tickets')class="active"@endif><a href="{{ route('project_tickets', ['id' => $project->id]) }}">{{ trans('gateway::project.tickets') }}</a></li>
+            <li @if ($active == 'monitoring')class="active"@endif><a href="{{ route('project_monitoring', ['id' => $project->id]) }}">Monitoring</a></li>
             <li><a>Référencement</a></li>
             <li><a>Planning</a></li>
-            <li @if ($active == 'monitoring')class="active"@endif><a href="{{ route('project_monitoring', ['id' => $project->id]) }}">Monitoring</a></li>
             <li><a>Messages</a></li>
         </ul>
     </nav>
--- src/resources/views/project/monitoring.blade.php
+++ src/resources/views/project/monitoring.blade.php
@@ -3,24 +3,42 @@
 @section('content')
     @include('gateway::includes.project_bar', ['active' => 'monitoring'])
 
-    <div class="project-template">
-        <h1 class="page-header">{{ trans('gateway::project.summary') }}</h1>
+    <div class="monitoring-template">
+        <h1 class="page-header">{{ trans('gateway::project.monitoring') }}</h1>
 
-        <h3>{{ trans('gateway::project.monitoring') }}</h3>
+        <div class="row" style="margin-top: 5rem">
+            <div id="loading_time" class="col-md-8"></div>
+            <div id="key_numbers" class="col-md-2 col-md-offset-1">
+                <span>Synthèse</span>
 
-        <p>Nombre de requêtes : {{ count($requests) }}</p>
+                <div class="content">
+                    <ul>
+                        <li>Pourcentage de disponibilité : <span class="number" style="color: #57c17b">{{ number_format($status_codes['20x'], 2) }}%</span></li>
+                        <li>Temps de chargement moyen : <span class="number" style="color: #57c17b">{{ number_format($average_loading_time, 2) }}s</span></li>
+                        <li>Plus long temps de chargement : <span class="number" style="color: #fc9a24">{{ number_format($max_loading_time, 2) }}s</span></li>
+                        <li>Nombre de requêtes : <span class="number">{{ count($requests) }}</span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
 
-        <p>Temps de chargement moyen : {{ number_format($average_loading_time, 2) }} s</p>
-
-        <div id="loading_time" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
-        <div id="status_codes" style="width: 50%; height: 350px; margin-top: 10rem"></div>
+        <div class="row" style="margin-top: 5rem">
+            <div id="status_codes_pie" class="col-md-3"></div>
+            <div id="loading_times_pie" class="col-md-3"></div>
+        </div>
     </div>
 
     <script>
         var data = {!! json_encode($requests) !!};
+
         var status_code_20x = {{ $status_codes['20x'] }};
         var status_code_40x = {{ $status_codes['40x'] }};
         var status_code_50x = {{ $status_codes['50x'] }};
+
+        var loading_times_below1 = {{ $loading_times['below1'] }};
+        var loading_times_between1and1_5 = {{ $loading_times['between1and1.5'] }};
+        var loading_times_between1_5and3 = {{ $loading_times['between1.5and3'] }};
+        var loading_times_morethan3 = {{ $loading_times['morethan3'] }};
 
         $(function () {
             $('#loading_time').highcharts({
@@ -54,12 +72,15 @@
                 series: [{
                     name: 'Webaccess',
                     data: data
-                }]
+                }],
+                credits: {
+                    enabled: false
+                }
             });
         });
 
         $(function () {
-            $('#status_codes').highcharts({
+            $('#status_codes_pie').highcharts({
                 chart: {
                     plotBackgroundColor: null,
                     plotBorderWidth: null,
@@ -94,12 +115,66 @@
                         y: status_code_20x
                     }, {
                         name: '40x - Erreur client',
-                        y: status_code_40x,
+                        y: status_code_40x
                     }, {
                         name: '50x - Erreur serveur',
-                        y: status_code_50x,
+                        y: status_code_50x
                     }]
-                }]
+                }],
+                credits: {
+                    enabled: false
+                }
+            });
+        });
+
+        $(function () {
+            $('#loading_times_pie').highcharts({
+                chart: {
+                    plotBackgroundColor: null,
+                    plotBorderWidth: null,
+                    plotShadow: false,
+                    type: 'pie'
+                },
+                title: {
+                    text: 'Temps de chargement de la page d\'accueil'
+                },
+                tooltip: {
+                    pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                },
+                plotOptions: {
+                    pie: {
+                        allowPointSelect: true,
+                        cursor: 'pointer',
+                        dataLabels: {
+                            enabled: true,
+                            format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                            style: {
+                                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                            }
+                        },
+                        colors: ['#57c17b', '#76D295', '#fc9a24', '#DF5A49']
+                    }
+                },
+                series: [{
+                    name: 'Temps de chargement',
+                    colorByPoint: true,
+                    data: [{
+                        name: 'Moins de 1s',
+                        y: loading_times_below1
+                    }, {
+                        name: 'Entre 1 et 1.5s',
+                        y: loading_times_between1and1_5
+                    }, {
+                        name: 'Entre 1.5 et 3s',
+                        y: loading_times_between1_5and3
+                    }, {
+                        name: 'Plus de 3s',
+                        y: loading_times_morethan3
+                    }]
+                }],
+                credits: {
+                    enabled: false
+                }
             });
         });
     </script>
