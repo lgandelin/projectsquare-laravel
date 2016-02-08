3f5fd32979da005a9c5b2c735bf1a90952fee5ae
Louis Gandelin <lgandelin@web-access.fr>
2016-02-08 14:36:31 +0100

    Requests are now inserted in database

---
+++ src/database/migrations/2016_02_08_131656_create_requests_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('requests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('project_id')->nullable();
+            $table->integer('status_code')->nullable();
+            $table->double('loading_time', 7, 5)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('requests');
+    }
+}
