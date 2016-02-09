4cc1903c591757ea0e50a2dde51d09b18c2ff399
Louis Gandelin <lgandelin@web-access.fr>
2016-02-09 17:53:35 +0100

    Added Setting model and migration

--- src/resources/views/project/settings.blade.php
+++ src/resources/views/project/settings.blade.php
@@ -7,19 +7,6 @@
         <h1 class="page-header">{{ trans('gateway::project.settings') }}</h1>
 
         <form action="{{ route('project_settings', ['id' => $project->id]) }}" method="post">
-            <div class="form-group">
-                <label for="name">{{ trans('gateway::settings.ping_request_frequency') }}</label>
-                <select class="form-control" name="ping_request_frequency">
-                    <option value="">{{ trans('gateway::generic.choose_value') }}</option>
-                    <option value="1">Toutes les minutes</option>
-                    <option value="2">Toutes les 5 minutes</option>
-                    <option value="3">Toutes les 15 minutes</option>
-                    <option value="4">Toutes les 30 minutes</option>
-                    <option value="5">Toutes les heures</option>
-                    <option value="6">Toutes les 4 heures</option>
-                    <option value="7">Tous les jours</option>
-                </select>
-            </div>
 
             <div class="form-group">
                 <button type="submit" class="btn btn-success">
@@ -29,6 +16,5 @@
                 <a href="{{ route('projects_index') }}" class="btn btn-default"><i class="glyphicon glyphicon-arrow-left"></i> {{ trans('gateway::generic.back') }}</a>
             </div>
         </form>
-
     </div>
 @endsection
