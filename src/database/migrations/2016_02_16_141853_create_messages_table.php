909910c42d7deca093e7b701e3906869029e8fd1
Louis Gandelin <lgandelin@web-access.fr>
2016-02-16 15:27:47 +0100

    Added Message and Conversation models + migrations

---
+++ src/database/migrations/2016_02_16_141853_create_messages_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('content')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->integer('conversation_id')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('messages');
+    }
+}
