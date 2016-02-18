ee805d099db8f008a9388c8bb301709f860f1bce
Louis Gandelin <lgandelin@web-access.fr>
2016-02-18 14:33:18 +0100

    Started conversations list

--- src/resources/views/messages/index.blade.php
+++ src/resources/views/messages/index.blade.php
@@ -8,5 +8,16 @@
 
     <div class="page-header">
         <h1>Messages</h1>
+
+        <table class="table table-striped table-responsive">
+            @foreach ($conversations as $conversation)
+                <tr>
+                    <td>{{ $conversation->id }}</td>
+                    <td>{{ date('d/m/Y H:i', strtotime($conversation->created_at)) }}</td>
+                    <td>{{ $conversation->project->name }}</td>
+                    <td>{{ $conversation->title }}</td>
+                </tr>
+            @endforeach
+        </table>
     </div>
 @endsection
