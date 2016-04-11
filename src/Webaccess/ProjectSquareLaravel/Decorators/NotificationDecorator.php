292890ca8b93cf1ef1af5d3764137ff85f0f6597
Louis Gandelin <lgandelin@web-access.fr>
2016-04-11 09:09:29 +0200

    Moved notifications JS code into global.js. PSR fixes

--- src/Webaccess/ProjectSquareLaravel/Decorators/NotificationDecorator.php
+++ src/Webaccess/ProjectSquareLaravel/Decorators/NotificationDecorator.php
@@ -9,19 +9,21 @@ class NotificationDecorator
 {
     public function decorate($notifications)
     {
-        foreach ($notifications as $notification) {
-            $notification->time = $notification->time->format('d/m/Y H:i');
-            if ($notification->type == 'EVENT_CREATED') {
-                $event = app()->make('GetEventInteractor')->execute(new GetEventRequest([
-                    'eventID' => $notification->entityID
-                ]));
-                $notification->link = route('calendar');
-                $notification->event_name = $event->name;
-            } elseif ($notification->type == 'MESSAGE_CREATED') {
-                $message = (new EloquentMessageRepository())->getMessage($notification->entityID);
-                $user = app()->make('UserManager')->getUser($message->userID);
-                $notification->link = route('conversation', ['id' => $message->conversationID]);
-                $notification->author_name = $user->firstName . ' ' . $user->lastName;
+        if (is_array($notifications) && sizeof($notifications) > 0) {
+            foreach ($notifications as $notification) {
+                $notification->time = $notification->time->format('d/m/Y H:i');
+                if ($notification->type == 'EVENT_CREATED') {
+                    $event = app()->make('GetEventInteractor')->execute(new GetEventRequest([
+                        'eventID' => $notification->entityID
+                    ]));
+                    $notification->link = route('calendar');
+                    $notification->event_name = $event->name;
+                } elseif ($notification->type == 'MESSAGE_CREATED') {
+                    $message = (new EloquentMessageRepository())->getMessage($notification->entityID);
+                    $user = app()->make('UserManager')->getUser($message->userID);
+                    $notification->link = route('conversation', ['id' => $message->conversationID]);
+                    $notification->author_name = $user->firstName . ' ' . $user->lastName;
+                }
             }
         }
 
