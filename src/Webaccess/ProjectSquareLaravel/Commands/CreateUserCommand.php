be9e5f488a320a8ea7ad9a484483b1d0ecfa1573
Louis Gandelin <lgandelin@web-access.fr>
2016-05-11 11:06:03 +0200

    Fixed a bug with the Create User command

--- src/Webaccess/ProjectSquareLaravel/Commands/CreateUserCommand.php
+++ src/Webaccess/ProjectSquareLaravel/Commands/CreateUserCommand.php
@@ -3,6 +3,7 @@
 namespace Webaccess\ProjectSquareLaravel\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Hash;
 
 class CreateUserCommand extends Command
 {
@@ -15,10 +16,10 @@ class CreateUserCommand extends Command
         $firstName = $this->ask('Entrez le prénom de l\'utilisateur');
         $lastName = $this->ask('Entrez le nom de l\'utilisateur');
         $email = $this->ask('Entrez l\'email de l\'utilisateur');
-        $password = $this->ask('Entrez le mot de passe de l\'utilisateur');
+        $password = $this->secret('Entrez le mot de passe de l\'utilisateur');
 
         try {
-            app()->make('UserManager')->createUser($firstName, $lastName, $email, $password, null);
+            app()->make('UserManager')->createUser($firstName, $lastName, $email, Hash::make($password), null);
         } catch (\Exception $e) {
             $this->error('Une erreur est survenue lors de l\'ajout de l\'utilisateur');
         }
