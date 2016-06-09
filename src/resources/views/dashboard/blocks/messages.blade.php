f100bf5e06b2966784d5028a5c36d1f3b2928b3a
Blandine Cortes <bcortes@web-access.fr>
2016-06-09 12:03:44 +0200

    Continued css dashboard	 (last message)

--- src/resources/views/dashboard/blocks/messages.blade.php
+++ src/resources/views/dashboard/blocks/messages.blade.php
@@ -9,8 +9,10 @@
                     <tr>
                         <!--<th>#</th>-->    
                         <th>{{ trans('projectsquare::tickets.client') }} </th>
-                        <th>{{ trans('projectsquare::messages.last_message') }} </th>
-                         <th>{{ trans('projectsquare::messages.date') }} </th>
+                        <th>{{ trans('projectsquare::messages.message') }} </th>
+                        <th>{{ trans('projectsquare::messages.allocated_user') }} </th>
+                        <th>{{ trans('projectsquare::messages.date') }} </th>
+                        <th>{{ trans('projectsquare::messages.action') }} </th>
                      
                     </tr>
                 </thead>
@@ -18,26 +20,43 @@
                 <tbody>
                 @foreach ($conversations as $conversation)
                     <tr class="conversation">
+                   
+                        <td align="center" style="border-left: 10px solid {{ $conversation->project->color }}">
+                        <span class="label text">{{ $conversation->project->client->name }}</span><br />{{ $conversation->title }}</strong><br></td>
+
                         <td>
-                            <td align="center" style="border-left: 10px solid {{ $conversation->project->color }}">
-                            <a href="{{ route('project_index', ['id' => $conversation->project->id]) }}"><span class="label" style="background: {{ $conversation->project->color }}">{{ $conversation->project->client->name }}</span></a><strong>{{ $conversation->title }}</strong><br>
+                            <p class="text">{{ $conversation->messages[sizeof($conversation->messages) - 1]->content }}</p>
+                        </td>
 
-                            <div class="message">
-                                <span class="badge">{{ date('d/m/Y H:i', strtotime($conversation->messages[sizeof($conversation->messages) - 1]->created_at)) }}</span> <span class="glyphicon glyphicon-user"></span> <span class="user_name">{{ $conversation->messages[sizeof($conversation->messages) - 1]->user->complete_name }}</span><br/>
-                                <p class="content">{{ $conversation->messages[sizeof($conversation->messages) - 1]->content }}</p>
-                            </div>
+                        <td>
+                            @include('projectsquare::includes.avatar', [
+                                'id' => $conversation->messages[sizeof($conversation->messages) - 1]->user->id,
+                                'name' => $conversation->messages[sizeof($conversation->messages) - 1]->user->complete_name 
+                            ])
+                         
+                        </td>
 
+                        <td>
+                            <span class="label text">{{ date('d/m H:i', strtotime($conversation->messages[sizeof($conversation->messages) - 1]->created_at)) }}</span>
+                        </td>
+
+                        <td>
+                            <a href="{{ route('conversation', ['id' => $conversation->id]) }}" class="btn btn-sm btn-primary see-more"></a>
+
+                            <span class="submit">
+                                <button class="btn btn-sm btn-success pull-right reply-message" data-id="{{ $conversation->id }}" style="margin-right: 1rem;"><span class="glyphicon glyphicon-comment"></span></button>
+                            </span>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td colspan="5">
                             <div class="message-inserted"></div>
 
                             <div class="message new-message" style="display:none">
                                 <textarea class="form-control" placeholder="{{ trans('projectsquare::dashboard.your_message') }}" rows="4"></textarea>
                                 <button class="btn btn-sm btn-default pull-right cancel-message" data-id="{{ $conversation->id }}" style="margin-top:1.5rem"><span class="glyphicon glyphicon-arrow-left"></span> {{ trans('projectsquare::generic.cancel') }}</button>
                                 <button class="btn btn-sm btn-success pull-right valid-message" data-id="{{ $conversation->id }}" style="margin-top:1.5rem; margin-right: 1rem"><span class="glyphicon glyphicon-ok"></span> {{ trans('projectsquare::generic.valid') }}</button>
-                            </div>
-
-                            <div class="submit">
-                                <a href="{{ route('conversation', ['id' => $conversation->id]) }}" class="btn btn-sm btn-primary pull-right"><span class="glyphicon glyphicon-share-alt"></span> {{ trans('projectsquare::messages.see_message') }}</a>
-                                <button class="btn btn-sm btn-success pull-right reply-message" data-id="{{ $conversation->id }}" style="margin-right: 1rem;"><span class="glyphicon glyphicon-comment"></span> {{ trans('projectsquare::messages.reply_message') }}</button>
                             </div>
                         </td>
                     </tr>
