6b52491d6d6001eb0002e21e16815ba33d554d8c
Louis Gandelin <lgandelin@web-access.fr>
2016-07-04 12:03:08 +0200

    Added password confirmation when creating users

--- src/Webaccess/ProjectSquareLaravel/Http/Controllers/Agency/ClientController.php
+++ src/Webaccess/ProjectSquareLaravel/Http/Controllers/Agency/ClientController.php
@@ -5,6 +5,8 @@ namespace Webaccess\ProjectSquareLaravel\Http\Controllers\Agency;
 use Illuminate\Support\Facades\Input;
 use Webaccess\ProjectSquareLaravel\Http\Controllers\BaseController;
 
+class TwoPasswordsException extends \Exception {}
+
 class ClientController extends BaseController
 {
     public function clients()
@@ -105,18 +107,24 @@ class ClientController extends BaseController
     public function store_user()
     {
         try {
-            app()->make('UserManager')->createUser(
-                Input::get('first_name'),
-                Input::get('last_name'),
-                Input::get('email'),
-                Input::get('password'),
-                Input::get('mobile'),
-                Input::get('phone'),
-                Input::get('client_id'),
-                Input::get('client_role'),
-                false
-            );
-            $this->request->session()->flash('confirmation', trans('projectsquare::users.add_user_success'));
+            if (Input::get('password') != '' && Input::get('password') != Input::get('password_confirmation')) {
+                throw new TwoPasswordsException();
+            } else {
+                app()->make('UserManager')->createUser(
+                    Input::get('first_name'),
+                    Input::get('last_name'),
+                    Input::get('email'),
+                    Input::get('password'),
+                    Input::get('mobile'),
+                    Input::get('phone'),
+                    Input::get('client_id'),
+                    Input::get('client_role'),
+                    false
+                );
+                $this->request->session()->flash('confirmation', trans('projectsquare::users.add_user_success'));
+            }
+        } catch (TwoPasswordsException $e) {
+            $this->request->session()->flash('error', 'Les deux mots de passe ne correspondent pas');
         } catch (\Exception $e) {
             $this->request->session()->flash('error', trans('projectsquare::users.add_user_error'));
         }
