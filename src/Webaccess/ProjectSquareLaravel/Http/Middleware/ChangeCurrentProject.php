0212f4d23279995b4361308f8a320f5a9745c549
Louis Gandelin <lgandelin@web-access.fr>
2016-07-12 14:03:39 +0200

    Fixed an issue when changing project

--- src/Webaccess/ProjectSquareLaravel/Http/Middleware/ChangeCurrentProject.php
+++ src/Webaccess/ProjectSquareLaravel/Http/Middleware/ChangeCurrentProject.php
@@ -16,8 +16,7 @@ class ChangeCurrentProject
      */
     public function handle($request, Closure $next)
     {
-        $path = str_replace('/project/', '', $request->getPathInfo());
-        $projectID = substr($path, 0, 1);
+        $projectID = str_replace('/project/', '', $request->getPathInfo());
 
         if (!$request->session()->has('current_project') || ($request->session()->has('current_project') && $projectID != $request->session()->get('current_project')->id)) {
             if ($project = app()->make('ProjectManager')->getProject($projectID)) {
