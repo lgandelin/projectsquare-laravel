c1f5e3cc65e002415d2b2c309af94c98f64e8fef
Louis Gandelin <lgandelin@web-access.fr>
2016-07-22 10:55:13 +0200

    Added widgets order/width persistence

--- src/Webaccess/ProjectSquareLaravel/Http/Controllers/DashboardController.php
+++ src/Webaccess/ProjectSquareLaravel/Http/Controllers/DashboardController.php
@@ -10,7 +10,19 @@ class DashboardController extends BaseController
 {
     public function index()
     {
+        if (!isset($_COOKIE['dashboard-widgets'])) {
+            $widgets = [
+                ['name' => 'tasks', 'width' => 12],
+                ['name' => 'tickets', 'width' => 7],
+                ['name' => 'messages', 'width' => 5],
+                ['name' => 'planning', 'width' => 12],
+            ];
+            $_COOKIE['dashboard-widgets'] = json_encode($widgets);
+        }
+        $widgets = json_decode($_COOKIE['dashboard-widgets']);
+
         return view('projectsquare::dashboard.index', [
+            'widgets' => $widgets,
             'tasks' => app()->make('GetTasksInteractor')->execute(new GetTasksRequest()),
             'tickets' => app()->make('GetTicketInteractor')->getTicketsPaginatedList($this->getUser()->id, env('TICKETS_PER_PAGE', 10)),
             'conversations' => $this->isUserAClient() ? app()->make('ConversationManager')->getConversationsByProject($this->getCurrentProject()->id, 5) : app()->make('ConversationManager')->getConversations($this->getUser()->id, 5),
