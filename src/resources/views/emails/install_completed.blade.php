0cde0eff33beea317c873c5f0d9e81a031a29418
Louis Gandelin <lgandelin@web-access.fr>
2016-08-25 10:50:26 +0200

    Fixed a typo when sending the email after the platform installation

--- src/resources/views/emails/install_completed.blade.php
+++ src/resources/views/emails/install_completed.blade.php
@@ -1,6 +1,6 @@
 {{ trans('projectsquare::email.install_project_1') }} <strong>{{ trans('projectsquare::email.platform_title') }}</strong> {{ trans('projectsquare::email.install_project_2') }} <br/><br/>
 
-{{ trans('projectsquare::email.access_project_1') }} <a href="http://{{ $url }}"><strong>{{ trans('projectsquare::email.access_project_1') }}</strong></a>.<br/><br/>
+{{ trans('projectsquare::email.platform_access_1') }} <a href="http://{{ $url }}"><strong>{{ trans('projectsquare::email.platform_access_2') }}</strong></a>.<br/><br/>
 
 {{ trans('projectsquare::email.signature_1') }}<br/>
 {{ trans('projectsquare::email.signature_2') }}<br/>
