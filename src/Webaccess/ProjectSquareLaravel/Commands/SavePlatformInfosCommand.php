d274cf6beb0b886d610125a5e6b8e72728450ed9
Louis Gandelin <lgandelin@web-access.fr>
2016-10-03 16:38:05 +0200

    Improved SavePlatformInfosCommand class

--- src/Webaccess/ProjectSquareLaravel/Commands/SavePlatformInfosCommand.php
+++ src/Webaccess/ProjectSquareLaravel/Commands/SavePlatformInfosCommand.php
@@ -5,26 +5,29 @@ namespace Webaccess\ProjectSquareLaravel\Commands;
 use Illuminate\Console\Command;
 use Webaccess\ProjectSquareLaravel\Models\Platform;
 
-class SavePlatformURLCommand extends Command
+class SavePlatformInfosCommand extends Command
 {
-    protected $signature = 'projectsquare:save-platform-url {url}';
+    protected $signature = 'projectsquare:save-platform-infos {url} {users_limit?}';
 
     protected $description = 'Sauvegarde l\'URL de la plateforme en base de données';
 
     public function handle()
     {
         $url = $this->argument('url');
-        $platform = new Platform();
+        $usersLimit = $this->argument('users_limit') ? $this->argument('users_limit') : 99;
+        $platform = (Platform::first()) ? Platform::first() : new Platform();
         $platform->url = $url;
+        $platform->users_limit = $usersLimit;
         $platform->save();
 
-        $this->info('URL de la plateforme sauvegardée avec succès');
+        $this->info('Informations de la plateforme sauvegardées avec succès');
     }
 
     protected function getArguments()
     {
         return [
             ['url', InputArgument::REQUIRED, 'The URL of the platform'],
+            ['users_limit', InputArgument::OPTIONAL, 'The users limit the platform'],
         ];
     }
 }
