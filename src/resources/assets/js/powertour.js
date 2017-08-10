f0a0f036e47102e672301012fe12fb1abb0b3b26
Louis Gandelin <lgandelin@web-access.fr>
2017-08-10 16:16:21 +0200

    Continued tutorial

---
+++ src/resources/assets/js/powertour.js
@@ -0,0 +1,108 @@
+$(document).ready(function() {
+
+    $.powerTour({
+        tours : [
+            {
+                trigger            : '',
+                startWith          : 1,
+                easyCancel         : true,
+                escKeyCancel       : true,
+                scrollHorizontal   : false,
+                keyboardNavigation : true,
+                loopTour           : false,
+                highlightStartSpeed : 200,
+                highlightEndSpeed  : 200,
+                onStartTour        : function(ui){ },
+                onEndTour          : function(ui){
+                    $('html, body').animate({
+                        scrollTop: 0
+                    }, 'slow');
+                },
+                onProgress         : function(ui){ },
+                steps : [
+                    {
+                        content         : '#step-intro',
+                        width           : 430,
+                        position        : 'sc',
+                        offsetY         : 50,
+                        highlight       : true,
+                    },
+                    {
+                        hookTo          : '#left-bar',
+                        content         : '#step-left-bar',
+                        width           : 370,
+                        position        : 'rt',
+                        offsetX         : 25,
+                        offsetY         : 250,
+                        highlight       : true,
+                    },
+                    {
+                        hookTo          : '#top-bar',
+                        content         : '#step-top-bar',
+                        width           : 470,
+                        position        : 'br',
+                        offsetY         : 25,
+                        offsetX         : 25,
+                        highlight       : true,
+                    },
+                    {
+                        hookTo          : '#tickets-widget',
+                        content         : '#step-tickets-widget',
+                        width           : 540,
+                        position        : 'rm',
+                        offsetX         : 15,
+                        highlight       : true,
+                    },
+                    {
+                        hookTo          : '#messages-widget',
+                        content         : '#step-messages-widget',
+                        width           : 400,
+                        position        : 'lm',
+                        offsetX         : 15,
+                        highlight       : true,
+                    },
+                    {
+                        hookTo          : '#tasks-widget',
+                        content         : '#step-tasks-widget',
+                        width           : 490,
+                        position        : 'bm',
+                        scrollSpeed     : 400,
+                        offsetX         : 15,
+                        highlight       : true,
+                    },
+                    {
+                        hookTo          : '#planning-widget',
+                        content         : '#step-planning-widget',
+                        width           : 480,
+                        position        : 'bm',
+                        scrollSpeed     : 400,
+                        offsetX         : 15,
+                        highlight       : true,
+                    }
+                ],
+                stepDefaults : [
+                    {
+                        width           : 300,
+                        position        : 'tr',
+                        offsetY         : 0,
+                        offsetX         : 0,
+                        fxIn            : 'fadeIn',
+                        fxOut           : 'fadeOut',
+                        showStepDelay   : 0,
+                        center          : 'step',
+                        scrollSpeed     : 250,
+                        scrollEasing    : 'swing',
+                        scrollDelay     : 0,
+                        timer           : '00:00',
+                        highlight       : true,
+                        keepHighlighted : false,
+                        keepVisible     : false,// new 2.2.0
+                        onShowStep      : function(ui){ },
+                        onHideStep      : function(ui){ }
+                    }
+                ]
+            }
+        ]
+    });
+    $.powerTour('run',1);
+});
