1a3cffa313c4dc66c40c8af9f2061ca161b1e6e1
Louis Gandelin <lgandelin@web-access.fr>
2017-09-13 10:36:07 +0200

    Improved the email sent when a new user is created

--- src/resources/views/emails/user_account_created.blade.php
+++ src/resources/views/emails/user_account_created.blade.php
@@ -1,11 +1,11 @@
+{{ trans('projectsquare::email.hello') }} {{ $first_name }},<br><br>
+
 {{ trans('projectsquare::email.account_created') }}<br/><br/>
+{{ trans('projectsquare::email.account_created_2') }}<br/><br>
 
+<strong>URL :</strong> <a href="http://{{ $url }}">{{ $url }}</a><br/>
 <strong>{{ trans('projectsquare::email.email') }}</strong> {{ $email }}<br/>
-<strong>{{ trans('projectsquare::email.password') }}</strong> {{ $password }}<br/>
-<strong>{{ trans('projectsquare::email.first_name') }}</strong> {{ $first_name }}<br/>
-<strong>{{ trans('projectsquare::email.name') }}</strong> {{ $last_name }}<br/><br/>
-
-{{ trans('projectsquare::email.platform_access_1') }} <a href="http://{{ $url }}"><strong>{{ trans('projectsquare::email.platform_access_2') }}</strong></a>.<br/><br/>
+<strong>{{ trans('projectsquare::email.password') }}</strong> {{ $password }}<br/><br>
 
 {{ trans('projectsquare::email.signature_1') }}<br/>
 {{ trans('projectsquare::email.signature_2') }}<br/>
