fbc0943cab7c85ec35e41b988737bf479e082da9
Louis Gandelin <lgandelin@web-access.fr>
2017-10-13 17:05:59 +0200

    Continued notifications

--- src/resources/assets/js/global.js
+++ src/resources/assets/js/global.js
@@ -64,56 +64,6 @@ $(document).ready(function() {
         $('.notifications').hide(200);
     });
 
-    //NOTIFICATIONS BOX
-    $('.notifications-link').click(function() {
-        $('.notifications').toggle(200);
-
-        if ($('.top-right-menu .notifications .content-tab[data-content="1"]').children('.notification').length == 0 && $('.top-right-menu .notifications .content-tab[data-content="2"]').children('.notification').length > 0) {
-            $('.top-right-menu .notifications .tabs li[data-tab="2"]').trigger('click');
-        }
-
-        $('.todos').hide(200);
-    });
-
-    $('.top-right-menu .notifications .tabs li').click(function() {
-        $('.top-right-menu .notifications .content-tab').hide();
-        $('.top-right-menu .notifications .tabs li').removeClass('current');
-
-        var tab = $(this).data('tab');
-        $('.top-right-menu .notifications .tabs li[data-tab="' + tab + '"]').addClass('current');
-        $('.top-right-menu .notifications .content-tab[data-content="' + tab + '"]').show();
-    });
-
-    $('.notifications').on('click', '.notification .notification-status', function() {
-        $(this).toggleClass('read', 'not-read');
-
-        var notification = $(this).closest('.notification');
-        var data = {
-            id: notification.data('id'),
-            _token: $('#csrf_token').val()
-        };
-
-        $.ajax({
-            type: "POST",
-            url: route_read_notification,
-            data: data,
-            success: function(data) {
-                notification.delay(750).slideUp(200).remove();
-                var notifications_count = parseInt($('.notifications-link').find('.badge').text());
-                notifications_count--;
-                $('.notifications-link').find('.badge').text(notifications_count);
-
-                if (notifications_count == 0) {
-                    $('.notifications-link').find('.badge').css('visibility', 'hidden');
-                }
-            }
-        });
-    });
-
-    $('.notifications .close').click(function() {
-        $('.notifications').hide(200);
-    });
-
     //DELETE BUTTONS
     $(document).on('click', '.btn-delete', function() {
         if (!confirm('Etes-vous sûrs de vouloir supprimer cet élément ?')) {
@@ -204,10 +154,6 @@ $(document).ready(function() {
         $(this).closest('.parent').find('.toggle-childs').toggleClass('glyphicon-triangle-bottom').toggleClass('glyphicon-triangle-top');
         $(this).closest('.parent').find('.childs').slideToggle();
     });
-
-    setTimeout(function() {
-        setInterval(reloadNotificationsPanel, 15000);
-    }, 15000);
 });
 
 function filter_projects_list() {
@@ -242,73 +188,4 @@ function createCookie(name,value) {
     date.setTime(date.getTime()+(365*24*60*60*1000));
     var expires = "; expires="+date.toGMTString();
     document.cookie = name+"="+value+expires+"; path=/";
-}
-
-function notify(author, body, icon, link)
-{
-    if('Notification' in window) {
-        Notification.requestPermission(function(permission) {
-            if (permission === 'granted') {
-                var notification = new Notification(author, {
-                    body: body,
-                    icon: icon,
-                    link: link
-                });
-
-                if (link) {
-                    notification.onclick = function () {
-                        window.open(link);
-                    };
-                }
-            }
-        });
-    }
-}
-
-function reloadNotificationsPanel() {
-    var data = {
-        _token: $('#csrf_token').val()
-    };
-
-    $.ajax({
-        type: "GET",
-        url: route_get_notifications,
-        data: data,
-        success: function(data) {
-            var notification_ids = [];
-            $('#top-bar .notifications .notification').each(function() {
-                notification_ids.push(parseInt($(this).data('id')))
-            });
-
-            var count = 0;
-            for (i in data.notifications) {
-                var notification = data.notifications[i];
-                count++;
-
-                if (!array_contains(notification_ids, notification.id)) {
-                    var html = loadTemplate('notification-template', notification);
-                    if (notification.type == 'MESSAGE_CREATED') {
-                        $('#top-bar .notifications .content-tab[data-content="2"]').prepend(html);
-                    } else {
-                        $('#top-bar .notifications .content-tab[data-content="1"]').prepend(html);
-                    }
-
-                    notify(notification.authorCompleteName, notification.title, notification.authorAvatar, notification.link);
-                }
-            }
-
-            if (count > 0) {
-                $('.notifications-link').find('.badge').text(count).css('visibility', 'visible');
-            }
-        }
-    });
-}
-
-function array_contains(array, obj) {
-    for (var i = 0; i < array.length; i++) {
-        if (array[i] === obj) {
-            return true;
-        }
-    }
-    return false;
 }
--- src/resources/views/includes/footer.blade.php
+++ src/resources/views/includes/footer.blade.php
@@ -10,7 +10,8 @@
     <script src="{{ asset('js/vendor/tooltipster.bundle.min.js') }}"></script>
     <script src="{{ asset('js/vendor/powertour.3.2.0.min.js') }}"></script>
     <script src="{{ asset('js/todos.js') }}"></script>
-    <script src="{{ asset('js/global.js') }}{{--?v={{ env('ASSETS_VERSION', '20170901') }}--}}"></script>
+    <script src="{{ asset('js/notifications.js') }}?v={{ env('ASSETS_VERSION', '20170901') }}"></script>
+    <script src="{{ asset('js/global.js') }}?v={{ env('ASSETS_VERSION', '20170901') }}"></script>
 
     @include('projectsquare::templates.new-notification')
 
